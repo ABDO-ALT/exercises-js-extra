cf3decd77efaee4e456eee02a96397742809ef1e
Abdo Mohamad <abdo@AiralkhasbAbdo.home>
2020-05-30 12:46:40 +0200

    Done exercises max

--- exercises/1-loops/3-max.js
+++ exercises/1-loops/3-max.js
@@ -3,8 +3,32 @@ Max
 Given an array of numbers, create a function to return the maximum 
 */
 
-function max(numbers){
-    //your code here
+//function max(numbers) {
+//your code here
+//console.log(numbers);
+// return Math.max(...numbers);
+//return Math.max.apply(null,numbers);
+//   let m = 0; // memory
+//   for (const number of numbers) {
+//     //console.log(number);
+//     //
+//     if(m < number){
+//       m = number;
+//     }
+//   }
+//   return  m
+// }
+​
+function max(numbers) {
+  //console.log(numbers);
+  let m = -Infinity;
+​
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0 && numbers[i] > m) {
+      m = numbers[i];
+    }
+  }
+  return m;
 }
 
 const numbersArray1 = [1,3,5,9];
