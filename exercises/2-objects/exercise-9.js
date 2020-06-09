0dd7b04ffb5ccb8e11a40d89d173c5fd0cf395dc
Abdo Mohamad <abdo@airalkhasbabdo.home>
2020-06-09 19:19:35 +0200

    Solution of exercise-9 done

--- exercises/2-objects/exercise-9.js
+++ exercises/2-objects/exercise-9.js
@@ -4,7 +4,7 @@ E-commerce
 You have to create an e-commerce shopping cart
 
 First of all we need 2 arrays, 
-the list of available products and the list of products introduced in the shopping cart.
+the list of available products and the list of products introduced in the shopping cart. x
 
 1. Fill the products list with at least 4 products, what attributes de we have? 
 at least should have:
@@ -33,33 +33,89 @@ var product1 = {
   id: 1,
   name: "Toaster X56 Plus",
   price: 12.98,
-  stock: 105
+  stock: 105,
 };
 var product2 = {
   id: 2,
   name: "Watch Rocker",
   price: 9.99,
-  stock: 2
+  stock: 2,
+};
+const product4 = {
+  id: 4,
+  name: "Watch",
+  price: 10,
+  stock: 4,
 };
 
 products.push(product1);
 products.push(product2);
+products.push(product4);
 
 var shoppingCart = {
   totalPrice: 0,
-  selectedProducts: []
+  selectedProducts: [],
 };
+// total Price 10
+// total Price 9.99
+// total Price 12.98
 
-function addToShoppingCart(id){
+// selectedProducts { product1 = [
+//   id: 1,
+//   name: "Toaster X56 Plus",
+//   price: 12.98,
+//   stock: 105,
+// };
+//  product2 = {
+//   id: 2,
+//   name: "Watch Rocker",
+//   price: 9.99,
+//   stock: 2,
+// };
+//  product4 = {
+//   id: 4,
+//   name: "Watch",
+//   product: 10,
+//   stock: 4,
+// }]
 
+function addToShoppingCart(id) {
+  //console.log("addtoShoppingId", id);
+  for (let i = 0; i < products.length; i++) {
+    //console.log(products[i]);
+    if (products[i].id === id) {
+      //console.log(products[i]);
+      shoppingCart.selectedProducts.push(products[i]);
+      //shoppingCart.totalPrice.push(products[i].price);
+      //console.log(products[i].price);
+      //console.log("sum", products[i].price);
+      //console.log("sum1", shoppingCart.totalPrice);
+      const sum = products[i].price + shoppingCart.totalPrice;
+      shoppingCart.totalPrice = sum;
+      // products[i].price;
+    }
+  }
 }
 
-function removeFromShoppingCart(id){
+function removeFromShoppingCart(id) {
+  for (let i = 0; i < shoppingCart.selectedProducts.length; i++) {
+    if (shoppingCart.selectedProducts[i].id === id) {
+      shoppingCart.selectedProducts.splice(i, 1);
+      shoppingCart.totalPrice -= shoppingCart.selectedProducts[i].price;
+      for (let j = 0; j < products.length; j++) {
+        if (products[j].id == id) {
+          products[j] += 1;
+        }
+      }
 
+      break;
+    }
+  }
 }
 
-function shop(){
-
+function shop() {
+  shoppingCart.totalPrice = 0;
+  shoppingCart.selectedProducts = [];
 }
 
 //results
@@ -67,24 +123,34 @@ addToShoppingCart(1);
 console.log("Step 1");
 console.log("Total Price = " + shoppingCart.totalPrice);
 console.log("Number of Elements = " + shoppingCart.selectedProducts.length);
-console.log("Name of Elements = " + shoppingCart.selectedProducts.map(p=>p.name));
+console.log(
+  "Name of Elements = " + shoppingCart.selectedProducts.map((p) => p.name)
+);
 addToShoppingCart(2);
 console.log("Step 2");
 console.log("Total Price = " + shoppingCart.totalPrice);
 console.log("Number of Elements = " + shoppingCart.selectedProducts.length);
-console.log("Name of Elements = " + shoppingCart.selectedProducts.map(p=>p.name));
+console.log(
+  "Name of Elements = " + shoppingCart.selectedProducts.map((p) => p.name)
+);
 addToShoppingCart(4);
 console.log("Step 3");
 console.log("Total Price = " + shoppingCart.totalPrice);
 console.log("Number of Elements = " + shoppingCart.selectedProducts.length);
-console.log("Name of Elements = " + shoppingCart.selectedProducts.map(p=>p.name));
+console.log(
+  "Name of Elements = " + shoppingCart.selectedProducts.map((p) => p.name)
+);
 removeFromShoppingCart(2);
 console.log("Step 4");
 console.log("Total Price = " + shoppingCart.totalPrice);
 console.log("Number of Elements = " + shoppingCart.selectedProducts.length);
-console.log("Name of Elements = " + shoppingCart.selectedProducts.map(p=>p.name));
+console.log(
+  "Name of Elements = " + shoppingCart.selectedProducts.map((p) => p.name)
+);
 shop();
 console.log("Step 5");
 console.log("Total Price = " + shoppingCart.totalPrice);
 console.log("Number of Elements = " + shoppingCart.selectedProducts.length);
-console.log("Name of Elements = " + shoppingCart.selectedProducts.map(p=>p.name));
+console.log(
+  "Name of Elements = " + shoppingCart.selectedProducts.map((p) => p.name)
+);
